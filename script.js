f6a37c95955b3c86559a62ea1424da5f602c00e1
BreidysG <Breidysgutierrez8@gmail.com>
2024-11-04 16:55:49 -0400

    Estilos responsivos

--- script.js
+++ script.js
@@ -41,8 +41,10 @@ function startTimer() {
 function displayCircles(circleNumber) {
     circleContainer.innerHTML = ''; // Limpiar contenedor
 
-    const sizes = [50, 100, 150, 200, 250]; // Tamaño de los círculos
-
+    let sizes = [50, 100, 150, 200, 250]; // Tamaño de los círculos
+    if (window.innerHeight <= 600) {
+        sizes = [75, 100, 130, 170, 200]; // Reducir alturas en dispositivos móviles
+    }
     // Crear círculo
     const circle = document.createElement('div');
     circle.classList.add('circulo'); // Agregar la clase 'circulo'
